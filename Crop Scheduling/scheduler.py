29b7ba713d09f97bbf59a83960bf4e10f5b31224
Bethany Christensen <a02299515@aggies.usu.edu>
2024-04-22 18:02:09 -0600

    Updated the plotting methods

--- Crop Scheduling/scheduler.py
+++ Crop Scheduling/scheduler.py
@@ -2,7 +2,6 @@ import math
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# TODO: verify spring/summer crops (don't trust github copilot)
 spring_crops = {
     "blue jazz": {"buy_price": 30, "intro_day": 0, "sell_price": 50, "harvest_rate": 1, "days_to_grow": 7, "regrow_days": 0},
     "cauliflower": {"buy_price": 80, "intro_day": 0, "sell_price": 175, "harvest_rate": 1, "days_to_grow": 12, "regrow_days": 0},
@@ -28,7 +27,6 @@ summer_crops = {
     "tomato": {"buy_price": 50, "intro_day": 0, "sell_price": 60, "harvest_rate": 1, "days_to_grow": 11, "regrow_days": 4},
     "wheat": {"buy_price": 10, "intro_day": 0, "sell_price": 25, "harvest_rate": 1, "days_to_grow": 4, "regrow_days": 0},
 }
-
 
 fall_crops = {
     "amaranth": {"buy_price": 70, "intro_day": 0, "sell_price": 150, "harvest_rate": 1, "days_to_grow": 7, "regrow_days": 0},
@@ -148,7 +146,6 @@ class Season:
             self.sell_days[day-1] = False
         
         self.current_day = 1
-        
 
     def get_crops(self):
         return self.crops
@@ -166,7 +163,11 @@ class Farm:
         self.money = 650
         self.field = []
         self.field_size = 100
-    
+        self.season_gains = []
+        self.gains = [self.money]
+        self.total_days = 0
+        self.color = 0
+        self.pallette = sns.color_palette("husl", 4)
 
     def tend(self):
         still_growing = []
@@ -185,7 +186,6 @@ class Farm:
         if self.season.sell_day():
             self.plant()
         
-        # water everything (update day)
         for crop in self.field:
             crop.water()
 
@@ -199,12 +199,13 @@ class Farm:
             valid_crops = get_valid_crops(crops, self.season.days - self.season.current_day, self.money)
             if len(valid_crops) > 0:
                 # define here what selection process to use
-                choice = get_most_profitable(valid_crops)
-                # choice = get_cheapest(valid_crops)
+                # choice = get_most_profitable(valid_crops)
+                choice = get_cheapest(valid_crops)
                 # choice = get_most_profitable_per_day(valid_crops)
-                # choice = get_smallest_grow_time(valid_crops)
+                # choice = get_smallest_grow_time(valid_crops) 
                 # choice = get_expensive(valid_crops)
                 # choice = get_highest_seasonal_profit(valid_crops)
+                # choice = get_longest_grow_time(valid_crops)
 
                 # buy seed and plant in field
                 self.money -= choice.buy_price
@@ -215,9 +216,12 @@ class Farm:
         print("DAY", self.season.current_day, "| MONEY: $", self.money)
         print("# of CROPS: ", len(self.field))
         self.tend()
-        self.plot()
         
-
+        # self.plot_money()
+        self.plot_crops()
+        self.total_days += 1
+        self.gains.append(self.money)
+        
         # for crop in self.field:
         #     print(crop)
 
@@ -227,9 +231,10 @@ class Farm:
             self.run_step()
 
         print()
-        print("FINAL MONEY: $", self.money)
+        print("FINAL MONEY: $", self.money)  
 
-    def plot(self):
+
+    def plot_crops(self):
         crop_names = [crop.name for crop in self.season.crops]
         field_crops = [crop.name for crop in self.field]
         crop_totals = [field_crops.count(crop) for crop in crop_names]
@@ -242,8 +247,28 @@ class Farm:
         plt.pause(0.08)
         plt.clf()
     
+
+    def plot_money(self, update=True):
+        sns.lineplot(x=range(self.total_days + 1), y=self.gains, label=self.season.name, color=self.pallette[self.color])
+        for gains in self.season_gains:
+            sns.lineplot(x=gains[0], y=gains[1], label=gains[2], color=gains[3])
+
+        plt.xlabel('Days')
+        plt.ylabel('Money')
+        plt.title(f'Money Over Time')
+        if update:
+            plt.pause(0.08)
+            plt.clf()
+        else:
+            plt.show()
+
     def change_seasons(self, season):
         print("SEASON CHANGE: ", season.name)
+        start = self.total_days - self.season.days
+        end = self.total_days + 1
+        self.season_gains.append([range(start, end, 1), self.gains[start:end], self.season.name, self.pallette[self.color]])
+        print(range(start, end, 1))
+        self.color += 1
         self.season = season
         keep = []
         for crop in self.field:
@@ -252,11 +277,11 @@ class Farm:
         
         self.field = keep
 
+
 def get_valid_crops(crops, days, money): 
     valid_crops = []
     for crop in crops:
         if crop.buy_price <= money and crop.days_to_grow <= days:
-            #TODO: make this not as clunky
             new_plant = Crop(crop.name, crop.intro_day, crop.harvest_rate, crop.days_to_grow, crop.regrow_days, crop.buy_price, crop.sell_price)
             valid_crops.append(new_plant)
     
@@ -269,6 +294,10 @@ def get_most_profitable(crops):
 
 def get_smallest_grow_time(crops):
     crops.sort(key=lambda x: x.days_to_grow)
+    return crops[0]
+
+def get_longest_grow_time(crops):
+    crops.sort(key=lambda x: x.days_to_grow, reverse=True)
     return crops[0]
 
 def get_most_profitable_per_day(crops):
@@ -309,7 +338,11 @@ summer = Season("summer", sum_crops, 100, [4,11,18,25,28])
 
 farm = Farm(spring)
 farm.run_simulation()
+
 farm.change_seasons(summer)
 farm.run_simulation()
+
 farm.change_seasons(fall)
 farm.run_simulation()
+
+farm.plot_money(update=False)
