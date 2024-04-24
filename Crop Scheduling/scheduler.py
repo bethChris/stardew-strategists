d8cd8388fe3026a1485211fd04d0cb22bf33fb1f
Bethany Christensen <a02299515@aggies.usu.edu>
2024-04-24 11:23:35 -0600

    updated readme for scheduling, added menu

--- Crop Scheduling/scheduler.py
+++ Crop Scheduling/scheduler.py
@@ -43,24 +43,6 @@ fall_crops = {
     "wheat": {"buy_price": 10, "intro_day": 0, "sell_price": 25, "harvest_rate": 1, "days_to_grow": 4, "regrow_days": 0},
 }
 
-# NOTES
-# assumed available energy increases 6 per day translating to an additional 3 field slots per day. Starting at 135 
-# max crops watered with basic level energy and watering can = 135 (270 max energy, 2 energy per water)
-# all sell prices are for 1 crop and is assumed to be the base sell price (anything more is a plus)
-# harvest rates are base rates with average of chance of getting more than 1 crop added
-# starter seeds is 15 parsnips (sell those and gain gold, then make decision) + 150 g
-# we only consider plants that can be be purchased and that are obtainable year 1
-# all prices based on pierre's shop as we support small businesses round here
-# giant crops are assumed to not be possible for simplicity
-# starter gold is 500 + 150 g from selling parsnips
-# all prices are for 1 seed
-# all seasons have 28 Days
-# assumed no speed grow
-# assumed no fertilizer
-# closed on wednesdays
-# assumed no crows
-
-
 # initial set up of seasons
 s_crops = []
 for crop in spring_crops:
@@ -83,14 +65,92 @@ fall = Season("fall", f_crops, 100, [4,11,16,18,25,27])
 seasons = [spring, summer, fall]
 
 
-# run simulation    
-farm = Farm(spring)
-farm.run_simulation()
+# NOTES
+# assumed available energy increases 6 per day translating to an additional 3 field slots per day. Starting at 135 
+# max crops watered with basic level energy and watering can = 135 (270 max energy, 2 energy per water)
+# all sell prices are for 1 crop and is assumed to be the base sell price (anything more is a plus)
+# harvest rates are base rates with average of chance of getting more than 1 crop added
+# starter seeds is 15 parsnips (sell those and gain gold, then make decision) + 150 g
+# we only consider plants that can be be purchased and that are obtainable year 1
+# all prices based on pierre's shop as we support small businesses round here
+# giant crops are assumed to not be possible for simplicity
+# starter gold is 500 + 150 g from selling parsnips
+# all prices are for 1 seed
+# all seasons have 28 Days
+# assumed no speed grow
+# assumed no fertilizer
+# closed on wednesdays
+# assumed no crows
 
-farm.change_seasons(summer)
-farm.run_simulation()
 
-farm.change_seasons(fall)
-farm.run_simulation()
+# function: displayMenu
+# purpose: display a menu with options
+# input: options - list of options to display
+#        descrs - list of descriptions for each option
+#        title - title of the menu
+# output: none
+def displayMenu(options, descrs, title="MENU"): 
+    longest_descr = max(len(max(descrs, key=len)), len(title))
+    longest_opt = len(max(options, key=len)) + 3
+    print()
+    print(f"{title:^{longest_descr+longest_opt}}")
 
-farm.plot_money(update=False)
+    print("-"*(longest_descr+longest_opt))
+    for opt, descr in zip(options, descrs):
+        print(f"[{opt}] {descr}")
+    print("-"*(longest_descr+longest_opt))
+    print()
+
+
+
+# function: driver
+# purpose: take user input and run the proper function
+# input: selection - integer representing the user's choice
+# output: none
+def driver(selection):
+    if selection == 1:
+        farm = Farm(spring)
+        farm.run_simulation()
+        farm.plot_money(update=False)
+    elif selection == 2:
+        farm = Farm(summer)
+        farm.run_simulation()
+        farm.plot_money(update=False)
+    elif selection == 3:
+        farm = Farm(fall)
+        farm.run_simulation()
+        farm.plot_money(update=False)
+    elif selection == 4:
+        farm = Farm(spring)
+        farm.run_simulation()
+        farm.change_seasons(summer)
+        farm.run_simulation()
+        farm.change_seasons(fall)
+        farm.run_simulation()
+        farm.plot_money(update=False)
+    else:
+        print("Invalid selection")
+
+
+# function: main
+# purpose: run the simulation
+# input: none
+# output: none
+def main():
+    # set up menu
+    questions = ["Run Spring", "Run Summer", "Run Fall", "Run Entire Year", "Quit"]
+    options = [str(i) for i in range(1, len(questions))]
+    options.append("q")
+    title = "STARDEW VALLEY CROP SIMULATION"
+    selection = ""
+
+    while selection != 'q':
+        displayMenu(options, questions, title)
+        selection = input("Please select an option: ").strip().lower()
+        if selection != 'q':
+            while selection not in options:
+                selection = input("Please select an option: ").strip().lower()
+            if selection != 'q':
+                driver(int(selection))
+        
+main()
